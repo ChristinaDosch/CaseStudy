fdf9ddc2fbdc7cdbcbfebc226271ce1da17e4cf1
sarahtum <sarah.braun@tum.de>
2016-05-02 09:36:49 +0200

    objective function for SO approach, using closed-form solution formula

---
+++ code RO/obj_SO_closed_form.m
@@ -0,0 +1,13 @@
+function [obj] = obj_SO_closed_form(x,H1,H2,cost,penalty,epsilon)
+%calculates the objective value using the closed-form formula for the
+%expected value E[F(,x,E)] as in (3) in section "1.2 Ansatz 1: Stochastic
+%Optimization" in objective.tex 
+%(however, we consider a min problem here, i.e. the formula is multiplied by -1)
+
+int1 = integral(H1,0,x);
+int2 = integral(H2,0,x);
+
+obj = - cost*(1+epsilon)*x + cost*(1+epsilon)*int1 + penalty((1-epsilon)*int2);
+
+end
+
