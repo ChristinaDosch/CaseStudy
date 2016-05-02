33673330f1d1dc0a8066d050cf2fd480ecddced3
sarahtum <sarah.braun@tum.de>
2016-05-02 09:45:48 +0200

    gradient in SO
    a gradient was added to the function obj_SO_closed_form

--- code RO/obj_SO_closed_form.m
+++ code RO/obj_SO_closed_form.m
@@ -1,4 +1,4 @@
-function [obj] = obj_SO_closed_form(x,H1,H2,cost,penalty,epsilon)
+function [obj,grad] = obj_SO_closed_form(x,H1,H2,cost,penalty,epsilon)
 %calculates the objective value using the closed-form formula for the
 %expected value E[F(,x,E)] as in (3) in section "1.2 Ansatz 1: Stochastic
 %Optimization" in objective.tex 
@@ -8,6 +8,8 @@ int1 = integral(H1,0,x);
 int2 = integral(H2,0,x);
 
 obj = - cost*(1+epsilon)*x + cost*(1+epsilon)*int1 + penalty((1-epsilon)*int2);
+
+grad = -cost*(1+epsilon) + cost*(1+epsilon)*H1(x) + penalty((1-epsilon)*H2(x));
 
 end
 
