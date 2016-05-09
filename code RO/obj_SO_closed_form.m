3a4f3dc292e59f918eb43d02985f468dd47d4b86
sarahtum <sarah.braun@tum.de>
2016-05-09 10:03:05 +0200

    comments changed
    the references to the documentation paper were adapted

--- code RO/obj_SO_closed_form.m
+++ code RO/obj_SO_closed_form.m
@@ -1,7 +1,7 @@
 function [obj,grad] = obj_SO_closed_form(x,H1,H2,cost,penalty,epsilon)
 %calculates the objective value using the closed-form formula for the
-%expected value E[F(,x,E)] as in (3) in section "1.2 Ansatz 1: Stochastic
-%Optimization" in objective.tex 
+%expected value E[F(x,E)] as in (14) in section "3.1 Ansatz 1: Stochastic
+%Optimization"
 %(however, we consider a min problem here, i.e. the formula is multiplied by -1)
 
 int1 = integral(H1,0,x);
