822da38647ee2bc4a15cabcdfe66293d75e93e40
sarahtum <sarah.braun@tum.de>
2016-05-16 18:27:48 +0200

    rename N
    N is renamed T (as in the documentation)

--- code model/constraints.m
+++ code model/constraints.m
@@ -1,4 +1,4 @@
-function [x_min, x_max, delta, A, b] = constraints(N)
+function [x_min, x_max, delta, A, b] = constraints(T)
 % returns bounds x_min,x_max,delta for constraints as well as matrix A and
 % vector b, describing all constraints in the form Ax<=b.
 % For detailled description of A and b see "Ansatz 2: Robust Optimization"
@@ -7,9 +7,9 @@ function [x_min, x_max, delta, A, b] = constraints(N)
 x_min = 0;  % minimum amount of power that can be scheduled
 x_max = 0.55; % maximum amount of power that can be scheduled
 delta = 0.04; % maximum deviation between two successive power values
-B = [-eye(N-1) zeros(N-1,1)] + [zeros(N-1,1) eye(N-1)];
+B = [-eye(T-1) zeros(T-1,1)] + [zeros(T-1,1) eye(T-1)];
 A = [B; -B];
-b = ones(2*(N-1),1)*delta;
+b = ones(2*(T-1),1)*delta;
 
 end
 
