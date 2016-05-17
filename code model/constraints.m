6da0e322e7da27b8031e358439c1f63fcf07073d
sarahtum <sarah.braun@tum.de>
2016-05-17 10:22:05 +0200

    improvements
    improved documentation and plots for SO

--- code model/constraints.m
+++ code model/constraints.m
@@ -2,7 +2,7 @@ function [x_min, x_max, delta, A, b] = constraints(T)
 % returns bounds x_min,x_max,delta for constraints as well as matrix A and
 % vector b, describing all constraints in the form Ax<=b.
 % For detailled description of A and b see "Ansatz 2: Robust Optimization"
-% input N indicates the number of time steps
+% input T indicates the number of time steps
 
 x_min = 0;  % minimum amount of power that can be scheduled
 x_max = 0.55; % maximum amount of power that can be scheduled
