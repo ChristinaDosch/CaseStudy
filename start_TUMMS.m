5e93d0d5a3f50eb9629a6e2c256f03dcb2c73a35
AlexeyPV <kuvshinovalexey@mail.ru>
2016-05-19 20:33:35 +0200

    Add files via upload

--- start_TUMMS.m
+++ start_TUMMS.m
@@ -1,18 +1,20 @@
-function start_TUMMS(x)
+function start_TUMMS
 %% Initialize parameters
 P = 20;
 N = 1 + 24*1; % one hour schedule
 t = linspace(0,24,N);
 x0 = 1;
-if nargin == 0, x = x0 + P*pdf(makedist('Normal',12,sqrt(12)),t); end
+str = load('Optimal x');
+x_opt = str.x_opt;
 
 cost = 1;
 penalty = 2; % linear penalty
-n = 5000; % number of the day simulations
+n = 10000; % number of the day simulations
 
 %% Initialize e_l, e_r
 pd_e = makedist('Normal',12,sqrt(12));
 e = x0 + P*pdf(pd_e,t); % gaussian radiance distribution during the day
+x = e;
 
 mu = 0.3; % variance of the gaussian distribution at each moment
 e_l = e - mu;
@@ -31,27 +33,33 @@ hMain = figure('SizeChangedFcn', @callback_size_change);
 s = hMain.Position;
 
 axMain = axes('Position', [0.05 0.1 0.925 0.75]);
-hPanel1 = uipanel('Title', 'Eingabe', 'Visible', 'off', 'Position', [0 0.9 1 0.1]);           % Eingabepanel
+hPanel = uipanel('Title', 'Eingabe', 'Visible', 'off', 'Position', [0 0.9 1 0.1]);           % Eingabepanel
 pos = linspace(100,s(3)-50,25);                                             
 for k = 1:N;
-    hEdit(k) = uicontrol('Style', 'edit', 'Parent', hPanel1, 'Visible', 'off', 'String', num2str(x(k)),...
+    hEdit(k) = uicontrol('Style', 'edit', 'Parent', hPanel, 'Visible', 'off', 'String', num2str(x(k)),...
                          'Position', [pos(k) 20 20 20],...
                          'Callback', @update_main_plot);
-    hLabel(k) = uicontrol('Style', 'text', 'Parent', hPanel1, 'Visible', 'off', 'String', [num2str(k-1) ':00'],...
+    hLabel(k) = uicontrol('Style', 'text', 'Parent', hPanel, 'Visible', 'off', 'String', [num2str(k-1) ':00'],...
                           'Position', [pos(k) 40 20 20]);
 end
 hSlider = uicontrol('Style', 'slider', 'Visible', 'off', 'BackgroundColor', 'white',...
                     'SliderStep', [1/n 100/n], 'Min', 1, 'Max', n, 'Value', 3,...
                     'Position', [0 0 100 20],...
                     'Callback', @update_main_plot);               
-hLabel_zeit = uicontrol('Style', 'text', 'Parent', hPanel1, 'Visible', 'off', 'String', 'Zeit',...
+hLabel_zeit = uicontrol('Style', 'text', 'Parent', hPanel, 'Visible', 'off', 'String', 'Zeit',...
                         'Position', [0 20 20 20]);
-hLabel_wert = uicontrol('Style', 'text', 'Parent', hPanel1, 'Visible', 'off', 'String', 'Wert',...
+hLabel_wert = uicontrol('Style', 'text', 'Parent', hPanel, 'Visible', 'off', 'String', 'Wert',...
                         'Position', [0 40 20 20]);
-hButton = uicontrol('Style', 'pushbutton', 'Parent', hPanel1, 'Visible', 'off', 'String', 'START',...
+hButton = uicontrol('Style', 'pushbutton', 'Parent', hPanel, 'Visible', 'off', 'String', 'START',...
                     'Position', [0 0 0 0],...
                     'Callback', @callback_startbutton);
-
+hButton_xsta = uicontrol('Style', 'pushbutton', 'Visible', 'off', 'String', 'sta',...
+                       'Position', [0 0 0 0],...
+                       'Callback', @callback_button_xsta);
+hButton_xopt = uicontrol('Style', 'pushbutton', 'Visible', 'off', 'String', 'opt',...
+                       'Position', [0 0 0 0],...
+                       'Callback', @callback_button_xopt);
+callback_size_change
 %% Callbacks
 function callback_startbutton(source, callbackdata)
     x = zeros(1,N);
@@ -73,6 +81,8 @@ function callback_size_change(source, callbackdata)
     hLabel_wert.Position = [0 5 50 20];
     hLabel_zeit.Position = [0 25 50 20];
     hButton.Position = [size(1)-50 5 40 40];
+    hButton_xsta.Position = [5 size(2)-85 30 20];
+    hButton_xopt.Position = [45 size(2)-85 30 20];
     for k = 1:N;
         object = hEdit(k);
         object.Position = [pos(k) 5 35 20];
@@ -82,11 +92,13 @@ function callback_size_change(source, callbackdata)
         object.Visible = 'on';
     end
     % Visibility
-    hPanel1.Visible = 'on';
+    hPanel.Visible = 'on';
     hLabel_wert.Visible = 'on';
     hLabel_zeit.Visible = 'on';
     hSlider.Visible = 'on';
     hButton.Visible = 'on';
+    hButton_xsta.Visible = 'on';
+    hButton_xopt.Visible = 'on';
 end
 function update_main_plot(source, callbackdata)
     x = zeros(1,N);
@@ -115,6 +127,26 @@ function update_main_plot(source, callbackdata)
     grid on
     hold off 
 end
+function callback_button_xsta(source, callbackdata)
+    x = e;
+    for k = 1:N
+        object = hEdit(k);
+        object.String = x(k);
+    end
+    [obj, i_bad, i_good] = calculate_objective;
+    
+    update_main_plot
+end
+function callback_button_xopt(source, callbackdata)
+    x = x_opt;
+    for k = 1:N
+        object = hEdit(k);
+        object.String = x(k);
+    end
+    [obj, i_bad, i_good] = calculate_objective;
+    
+    update_main_plot
+end
 function create_bestworst_plot
     figure,
     ax1 = axes;
@@ -126,8 +158,8 @@ function create_bestworst_plot
     plot(t, x, 'k*',...             % schedule
          [t; t], [e_l; e_u], 'k+-.')% uncertainty intervals
 
-    legend('Tag mit dem kleinsten Gewinn',...
-           'Tag mit dem groessten Gewinn',...
+    legend('Tag mit dem groessten Gewinn',...
+           'Tag mit dem kleinsten Gewinn',...
            'deine Vorhersage')
     xlabel('Zeit'), ylabel('Energie, MWh')
     title('Tage mit dem kleinsten und dem groessten Gewinn')
@@ -169,8 +201,8 @@ end
 function [obj, i_bad, i_good] = calculate_objective
     X = ones(n,1)*x;
     obj = sum(cost*E - max(zeros(n,N), X - E)*penalty - max(zeros(n,N), E - X)*cost, 2);
-    i_bad = find(obj == min(obj));
-    i_good = find(obj == max(obj));
+    i_bad = find(obj == min(obj), 1, 'first');
+    i_good = find(obj == max(obj), 1, 'first');
 end
 
 end
