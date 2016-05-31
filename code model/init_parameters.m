1197a952f876153c86213767803e97e416109d4a
sarahtum <sarah.braun@tum.de>
2016-05-31 08:49:35 +0200

    Add ReadMe

--- code model/init_parameters.m
+++ code model/init_parameters.m
@@ -1,8 +1,9 @@
 function [T, P, cost, penalty, epsilon, C, t, mu, sigma, lambda] = init_parameters
 
-%T = 1 + 24*1;       % one hour schedule
-T = 3;              % to try start_SO_closed with an acceptable runtime
-%T = 1440;            % every-minute schedule
+T = 1 + 24*1;        % one hour schedule (works for RO and start_SO_closed, for the latter only with long running time)
+%T = 3;              % for start_SO_closed with an acceptable runtime
+%T = 1440;           % every-minute schedule (required for start_SO_discretization and start_SO_discretization_battery)
+
 P = 3.8;             % nominal power of the PV element
 epsilon = 0.05;
 cost = 1;
