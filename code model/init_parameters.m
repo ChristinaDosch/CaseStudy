835bc27820a8082dae4b175bfa2359ef1c6202be
sarahtum <sarah.braun@tum.de>
2016-05-31 08:27:54 +0200

    changed init-files

--- code model/init_parameters.m
+++ code model/init_parameters.m
@@ -1,11 +1,13 @@
-function [T, P, cost, penalty, epsilon, t, mu, sigma, lambda] = init_parameters
+function [T, P, cost, penalty, epsilon, C, t, mu, sigma, lambda] = init_parameters
 
-%T = 1 + 24*1;        % one hour schedule
-T = 1440;            % every-minute schedule
+%T = 1 + 24*1;       % one hour schedule
+T = 3;              % to try start_SO_closed with an acceptable runtime
+%T = 1440;            % every-minute schedule
 P = 3.8;             % nominal power of the PV element
 epsilon = 0.05;
 cost = 1;
 penalty = @(x) x.^2;  % quadratic penalty
+C = 2.6;              % battery capacity
 t = linspace(0,24,T);
 
 pd = makedist('Normal');
