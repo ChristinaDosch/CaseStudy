10cf48ec10a12897019ddbc85c2aba8845cb97de
MartinaK3 <martina.kuchlbauer@gmx.de>
2016-06-09 18:31:27 +0200

    SO discretization smart approach
    adapted init_constraints, created start_SO_discr_smart

--- code model/init_constraints.m
+++ code model/init_constraints.m
@@ -1,4 +1,4 @@
-function [x_min, x_max, delta, A, b, A_, b_, A_smart, b_smart] = init_constraints(T, P, C, SOC_0)
+function [x_min, x_max, SOC_min, SOC_max, delta, A, b, A_, b_, A_smart, b_smart] = init_constraints(T, P, C, SOC_0)
 % returns bounds x_min,x_max,delta for constraints as well as matrix A and
 % vector b, describing all constraints in the form Ax<=b.
 % For detailled description of A and b see "Ansatz 2: Robust Optimization"
@@ -10,6 +10,8 @@ function [x_min, x_max, delta, A, b, A_, b_, A_smart, b_smart] = init_constraint
 
 x_min = 0;  % minimum amount of power that can be scheduled
 x_max = 0.7*P;  % maximum amount of power that can be scheduled
+SOC_min = 0.1; % maximum of state of charge
+SOC_max = 0.95; % minimum of state of charge
 delta = 0.03*P;    % maximum deviation between two successive power values
 B = [-eye(T-1) zeros(T-1,1)] + [zeros(T-1,1) eye(T-1)];
 A = [B; -B];
@@ -22,16 +24,16 @@ b_ = [C*ones(T,1); zeros(T,1)];
 % For SO with battery (SMART)
 B_tilde = [B, zeros(T-1,3*T)];
 C_smart = [zeros(T,T), eye(T) + diag(-ones(T-1,1),-1) , -0.95/C * eye(T), 1/C * eye(T)];
-D = [zeros(T,2*T), -eye(T), 0.95 * eye(T)];
+%D = [zeros(T,2*T), -eye(T), 0.95 * eye(T)];
 c = [SOC_0; zeros(T-1,1)];
 % d = ; %kann man hoffentlich weglassen
-A_smart = [B_tilde; -B_tilde; C_smart; -C_smart; D; -D];
-b_smart = [ones(T-1,1)*delta; -ones(T-1,1)*delta; c; -c; d; -d];
+A_smart = [B_tilde; C_smart; -C_smart];%; D; -D];
+b_smart = [ones(T-1,1)*delta; c; -c];%; d; -d];
 
 %TO DO:
 % - upper bounds und lower bounds (die ja nicht in Matrix mit drin stehen,
 %   weil Matlab mit solchen capacity constraints sinnvoller umgehen kann)
-%   müssen dann auch oben als function output noch rein
-% - d weglassen und damit auch D
+%   müssen dann auch oben als function output noch rein -erledigt
+% - d weglassen und damit auch D -erledigt
 end
 
--- code model/start_SO_discretization_battery_smart.m
+++ code model/start_SO_discretization_battery_smart.m
@@ -11,19 +11,19 @@ if nargin == 0, ToPlotOrNotToPlot = true; end
 [T, P, cost, penalty, epsilon, C, SOC_0, t] = init_parameters;
 
 %% Constraints
-[x_min,x_max,~,~,delta,A,b,~,~,~,~] = init_constraints(T,P,C,SOC_0);
+[x_min,x_max,SOC_min, SOC_max, delta,~,~,~,~,A_smart,b_smart] = init_constraints(T,P,C,SOC_0);
 
 %% Example scenarios
 
 % using SAMPLE_NORMAL_INDEPENDENT.CSV:
-% for this example T = 96 is required in init_parameters!!!
+% for this example T = 96 is required in init_parameters!!! (15min interv)
 E = load('sample_normal_independent.csv');
 E = 1/1000 * E; % since we need kWh (and in the samples it's in Wh)
 %K = size(E,1); % number of realizations
-K = 1;
+K = 300;
 
 % using PVDATA2.MAT
-% for this example T = 1440 is required in init_parameters!!! 
+% for this example T = 1440 is required in init_parameters!!! (1min interv)
 %E = load('PVdata2');
 %E = 1/1000 * E;
 %K = 372; % number of realizations
@@ -34,29 +34,37 @@ F = cell(K,1);
 
 % determine revenue function F(x,\tilde{x}^k) for every k=1,...,K:
 for k = 1:K 
-x_tilde = @(x) battery(E(k,:),x,C,SOC_0); % compute \tilde{x}^k
-F(k) = { @(x) obj_SO_discr(x,x_tilde(x),cost,penalty,epsilon,P)};
+x_tilde = @(x) E(k,:)+0.95*x((3*T+1):(4*T))-x((2*T+1):(3*T)); % compute \tilde{x}^k
+F(k) = { @(x) obj_SO_discr(x(1:T),x_tilde(x),cost,penalty,epsilon,P)};
 end
 
 objfct = @(x) 1/K * sum(cellfun(@(f)f(x),F)); % weighted (all weights=1/K) sum of F(x,e^k)
 
 %% Performing optimization
-x0 = zeros(1,T);
+x0 = zeros(1,4*T);
 tic
-[x_opt, obj_opt] = patternsearch(objfct,x0,A,b,[],[],x_min*ones(1,T),x_max*ones(1,T));
-%[x_opt, obj_opt] = fmincon(objfct,x0,A,b,[],[],x_min*ones(1,T),x_max*ones(1,T));
+%[x_opt, obj_opt] = patternsearch(objfct,x0,A_smart,b_smart,[],[],...
+ %   [x_min*ones(1,T), SOC_min*ones(1,T),0*ones(1,(2*T))],[x_max*ones(1,T), SOC_max*ones(1,T),2*P*ones(1,(2*T))]);
+[x_opt, obj_opt] = fmincon(objfct,x0,A_smart,b_smart,[],[],...
+    [x_min*ones(1,T), SOC_min*ones(1,T),0*ones(1,(2*T))],[x_max*ones(1,T), SOC_max*ones(1,T),2*P*ones(1,(2*T))]);
 runningTime = toc
 %% 4.) Plot the solutions and data
 if ToPlotOrNotToPlot
     figure, hold on,
-    plot(t,x_opt,'*r',... % solution computed by ga or patternsearch
-         t,x_opt + epsilon*P,'^r',...
-         t,x_opt - epsilon*P,'vr',...
+    plot(t,x_opt(1:T),'*r',... % solution computed by ga or patternsearch
+         t,x_opt(1:T) + epsilon*P,'^r',...
+         t,x_opt(1:T) - epsilon*P,'vr',...
+         t,x_opt(T+1:2*T)*C, 'bo',...
+         t,x_opt(2*T+1:3*T), '-b',...
+         t,x_opt(3*T+1:4*T), '-g',...
          [t(1) t(end)], [x_max x_max], 'k--',... % x_max
          [t(1) t(end)], [x_min x_min], 'k--') % x_min 
     legend('calculated opt. sol.',...
            'upper no-penalty bound',...
            'lower no-penalty bound',...
+           'SOC',...
+           'b in',...
+           'b out',...
            'x_{max}, x_{min}')
     xlabel('time'), ylabel('energy, kWh')
     % size
