8886e0f70aa8aad12aed8d7bff9e77d885641792
sarahtum <sarah.braun@tum.de>
2016-06-10 10:53:03 +0200

    revise ramping constraint
    ramping constraint is constraint on the absolute value of sth, i.e. it
    needs to be contained twice

--- code model/init_constraints.m
+++ code model/init_constraints.m
@@ -3,16 +3,18 @@ function [x_min, x_max, SOC_min, SOC_max, delta, A, b, A_, b_, A_smart, b_smart]
 % vector b, describing all constraints in the form Ax<=b.
 % For detailled description of A and b see "Ansatz 2: Robust Optimization"
 % 
-% input T indicates the number of time steps
-% input P indicates nominal power (needed since delta and x_max depend on
-% P)
-% input C: battery capacity
+% input:   T - scalar:           number of time steps
+%          P - scalar:           nominal power (needed since delta and x_max depend on P)
+%          C - scalar:           battery capacity
 
-x_min = 0;  % minimum amount of power that can be scheduled
+% capacity bounds:
+x_min = 0;      % minimum amount of power that can be scheduled
 x_max = 0.7*P;  % maximum amount of power that can be scheduled
-SOC_min = 0.1; % maximum of state of charge
+SOC_min = 0.1;  % maximum of state of charge
 SOC_max = 0.95; % minimum of state of charge
-delta = 0.03*P;    % maximum deviation between two successive power values
+
+% "real" constraints (not capacity constraints)
+delta = 0.03*P; % maximum deviation between two successive power values
 B = [-eye(T-1) zeros(T-1,1)] + [zeros(T-1,1) eye(T-1)];
 A = [B; -B];
 b = ones(2*(T-1),1)*delta;
@@ -24,11 +26,10 @@ b_ = [C*ones(T,1); zeros(T,1)];
 % For SO with battery (SMART)
 B_tilde = [B, zeros(T-1,3*T)];
 C_smart = [zeros(T,T), eye(T) + diag(-ones(T-1,1),-1) , -0.95/C * eye(T), 1/C * eye(T)];
-%D = [zeros(T,2*T), -eye(T), 0.95 * eye(T)];
 c = [SOC_0; zeros(T-1,1)];
-% d = ; %kann man hoffentlich weglassen
-A_smart = [B_tilde; C_smart; -C_smart];%; D; -D];
-b_smart = [ones(T-1,1)*delta; c; -c];%; d; -d];
+
+A_smart = [B_tilde; -B_tilde; C_smart; -C_smart];
+b_smart = [ones(T-1,1)*delta; ones(T-1,1)*delta; c; -c];
 
 %TO DO:
 % - upper bounds und lower bounds (die ja nicht in Matrix mit drin stehen,
