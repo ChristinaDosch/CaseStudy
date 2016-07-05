2de78f50c9ce4f7ee7d145be580e87c0bd243c1a
MartinaK3 <martina.kuchlbauer@gmx.de>
2016-07-05 11:05:11 +0200

    adapt SO plots for final

--- code model/SO plots final/plotCreator.m
+++ code model/SO plots final/plotCreator.m
@@ -26,8 +26,8 @@ x_opt_noBat(10) = 0;
 K = 3; 
 [T, P, cost, penalty, penalty_grad, epsilon, C, SOC_0, t, mu, sigma, lambda, penalty_hess] = init_parameters;
 [x_min, x_max, delta, SOC_min, SOC_max, A, b, A_, b_, A_smart, b_smart] = init_constraints(T, P, C, SOC_0,K);
-%E = load('sample_normal_independent.csv');
-%E = 1/1000 * max(E, 0); E_good(1,:) = E(2,:); E = E_good;
+E = load('sample_normal_independent.csv');
+E = 1/1000 * max(E, 0); E_good(1,:) = E(2,:); E = E_good;
 
 %% active ramping constraints
 % for marking active ramping constraints in battery plan
@@ -44,8 +44,8 @@ x_x_opt_noBat = x_opt_noBat(1:T);
 %% plot    
     figure, hold on,
     plot(t,x_opt_bat(1:T),'*b',... % solution computed by sqp with battery
-         t,x_opt_noBat(1:T),'*r',... % solution without battery % t,E(1,:),'xk',...% sample 1
-         t,mu,'-k',... % (estimated) expected value
+         t,x_opt_noBat(1:T),'*r',... % solution without battery % t,E(1,:),'xk',...% sample 1 %t,mu,'-k',... % (estimated) expected value
+         t,E(1,:),'-k',... %scenario
          [t(1) t(end)], [x_max x_max], 'k--',... % x_max
          [t; t], [zeros(1,T); x_opt_bat(T+K*T+1:T+K*T+T) - x_opt_bat(T+2*K*T+1:T+2*K*T+T)], '-b',... % b^in - b^out, sodass Strich nach oben, wenn Batterie beladen wird und Strich nach unten, wenn Batterie entladen wird
          [t(1) t(end)], [x_min x_min], 'k--',...) % x_min 
@@ -56,11 +56,11 @@ x_x_opt_noBat = x_opt_noBat(1:T);
      %t,x_opt(1:T) - epsilon*P,'vr',...    
      %t,x_opt(T+K*T+1:T+K*T+T), '-ob',... % \tilde{b^in,1}         
      %t,x_opt(T+2*K*T+1:T+2*K*T+T), '-og',... % \tilde{b^out,1} 
-    fs = 15;
+    fs = 16.5;
     set(gca,'FontSize',fs);
     legend1 = legend('optimal sol. with battery',...
            'optimal sol. without battery',...% 'first sample',...
-           'expected value',... 
+           'scenario',...%'expected value',... 
            'x_{max}, x_{min}',...
            'battery usage')
     set(legend1,'Fontsize',fs)
@@ -72,12 +72,11 @@ x_x_opt_noBat = x_opt_noBat(1:T);
     v = axis;
     ylim([-1 v(4)+0.5]);
     % info-box at the top left corner
-    text(0.03*v(2),0.98*(v(4)+0.5),['cost = ', num2str(cost(1))], 'Fontsize',fs)
-    text(0.03*v(2),0.935*(v(4)+0.5),'penalty = 7x^2', 'Fontsize',fs)
-    %text(0.05*v(2),0.87*v(4),['[x_{min}, x_{max}] = ', '[', num2str(x_min), ', ', num2str(x_max), ']'])
-    text(0.03*v(2),0.87*(v(4)+0.5),['\Delta = ', num2str(delta)], 'Fontsize',fs)
-    text(0.03*v(2),0.79*(v(4)+0.5),'optimal obj. value with battery = ', 'Fontsize',fs)
-    text(0.3*v(2),0.79*(v(4)+0.5), num2str(abs(obj_opt_bat)),'color','b', 'Fontsize',fs)
-    text(0.03*v(2),0.74*(v(4)+0.5),'optimal obj. value without battery = ', 'Fontsize',fs)
-    text(0.326*v(2),0.74*(v(4)+0.5),num2str(abs(obj_opt_noBat)),'color','r', 'Fontsize',fs)
+    %text(0.03*v(2),0.98*(v(4)+0.5),['cost = ', num2str(cost(1))], 'Fontsize',fs)
+    %text(0.03*v(2),0.935*(v(4)+0.5),'penalty = 7x^2', 'Fontsize',fs)
+    %text(0.03*v(2),0.87*(v(4)+0.5),['\Delta = ', num2str(delta)], 'Fontsize',fs)
+    text(0.03*v(2),0.95*(v(4)+0.5),'optimal obj. value with battery = ', 'Fontsize',fs)
+    text(0.33*v(2),0.95*(v(4)+0.5), num2str(abs(obj_opt_bat)),'color','b', 'Fontsize',fs)
+    text(0.03*v(2),0.885*(v(4)+0.5),'optimal obj. value without battery = ', 'Fontsize',fs)
+    text(0.36*v(2),0.885*(v(4)+0.5),num2str(abs(obj_opt_noBat)),'color','r', 'Fontsize',fs)
     %title('SO discretization approach with (blue) and without (red) battery','Fontsize',18)
